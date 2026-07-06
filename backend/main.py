89b92ffb7b76caee2a74f9dcf1405d51ccb8841d
Ishan Parab <ishan.parab.comp@gmail.com>
2026-07-06 13:31:25 +0530

    sidebar\

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ import boto3
 from supabase import create_client, Client
 from database import supabase
 
+
 from fastapi import FastAPI, UploadFile, File, BackgroundTasks, HTTPException
 from fastapi.responses import FileResponse
 from docx import Document
